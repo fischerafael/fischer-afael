33bec65004cd309b6332c6c40838c59289b8995f
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-12-20 21:06:10 -0300

    add social links

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,22 @@
-import { Avatar, Flex, HStack, Text, VStack } from "@chakra-ui/react";
+import {
+  Avatar,
+  Flex,
+  HStack,
+  IconButton,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { Header } from "../../components/organisms/Header";
 import { IPostRes } from "../../interfaces";
+import { FaLinkedin, FaInstagram, FaYoutube } from "react-icons/fa";
 
 export const PageHome = ({ posts }: { posts: IPostRes[] }) => {
+  const { push } = useRouter();
+  const onNavigateTo = (url: string) => {
+    push(url);
+  };
+
   return (
     <Flex
       w="full"
@@ -22,25 +35,60 @@ export const PageHome = ({ posts }: { posts: IPostRes[] }) => {
           justify="center"
           maxW="container.sm"
           align="center"
-          h="80vh"
+          minH="60vh"
           spacing="8"
+          py="8"
         >
           <VStack spacing="0" w="full" align="left">
-            <Text fontWeight="bold" fontSize="6xl" color="cyan.500">
+            <Text fontWeight="bold" fontSize="7xl" color="cyan.500">
               Front-End &
             </Text>
-            <Text fontWeight="bold" fontSize="6xl">
+            <Text fontWeight="bold" fontSize="7xl">
               UX Design &
             </Text>
-            <Text fontWeight="bold" fontSize="6xl">
+            <Text fontWeight="bold" fontSize="7xl">
               UI Design.
             </Text>
           </VStack>
           <Text fontSize="lg">
-            I help companies, entrepreneurs and developers to design, develop
-            and launch amazing digital products.
+            Hi, my name is Rafael, and I help companies, entrepreneurs and
+            developers to design, develop and launch amazing digital products.
           </Text>
         </VStack>
+
+        <HStack w="full" justify="center" spacing="4" h="20vh">
+          <IconButton
+            aria-label="Rafael's Linkedin"
+            icon={<FaLinkedin size="md" />}
+            variant="ghost"
+            color="cyan.500"
+            onClick={() =>
+              onNavigateTo("https://www.linkedin.com/in/fischerafael/")
+            }
+          />
+
+          <IconButton
+            aria-label="Rafael's Instagram"
+            icon={<FaInstagram size="md" />}
+            variant="ghost"
+            color="cyan.500"
+            onClick={() =>
+              onNavigateTo("https://www.instagram.com/fischerafael/")
+            }
+          />
+
+          <IconButton
+            aria-label="Rafael's Youtube"
+            icon={<FaYoutube size="md" />}
+            variant="ghost"
+            color="cyan.500"
+            onClick={() =>
+              onNavigateTo(
+                "https://www.youtube.com/channel/UCz75xXTimMhZkbK__XOM8hQ"
+              )
+            }
+          />
+        </HStack>
       </VStack>
     </Flex>
   );
